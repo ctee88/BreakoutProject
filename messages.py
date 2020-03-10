068201a5dd433f2e3c69629c82a802d618bb42bb
ctee88 <61874412+ctee88@users.noreply.github.com>
2020-03-10 18:36:38 +0000

    Added docstring explanations

--- messages.py
+++ messages.py
@@ -15,15 +15,20 @@ class Messages():
 		self.font_colour = (255, 255, 255)
 		self.font = pygame.font.SysFont("Helvetica", 18)
 		
-		#Value for y co-ordinate for the messsages
+		#Value for y co-ordinate for the drawn messages
 		self.y_pos = 250
 		
 		self.prep_start()
 		self.prep_game_over()
-	
+		
+		"""
+		Purpose of prep_ methods:
+		Change the values into text so that they can be changed into
+		images which can then be drawn to the screen.
+		"""
+		
 	def prep_start(self):
-		#Turn start msg into an image and position it slightly lower than the
-		#screen's centre
+		#Turn start msg into an image and position it slightly lower than the screen's centre
 		start_str = "Press SPACE to launch ball"
 		self.start_image = self.font.render(start_str, True, self.font_colour,
 			self.settings.bg_colour)
@@ -33,9 +38,8 @@ class Messages():
 		self.start_rect.y = self.y_pos
 		
 	def prep_game_over(self):
-		#Turn game over msg into an image and position it slightly lower than
-		#the screen's centre
-		game_over_str = "GAME OVER! Press SPACE to play again or Q to quit"
+		#Turn game over msg into an image and position it slightly lower than the screen's centre
+		game_over_str = "GAME OVER! Press SPACE to restart or Q to quit"
 		self.game_over_image = self.font.render(game_over_str, True, self.font_colour,
 			self.settings.bg_colour)
 		
