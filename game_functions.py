1265f89b22e143502962708beede3a21eb40a1c6
ctee88 <61874412+ctee88@users.noreply.github.com>
2020-03-10 18:31:20 +0000

    Fixed issue with game over (see Limitations)
    Game over message correctly drawn (at the right time) and the game correctly resets the stats and displayed the reset stats after the player presses the SPACE button.

--- game_functions.py
+++ game_functions.py
@@ -6,17 +6,17 @@ import sys
 import pygame
 from brick import Brick
 
-def check_events(paddle, ball, settings, stats, msg):
+def check_events(paddle, ball, bricks, screen, settings, sb, stats, msg):
 	#Check for KB and mouse events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 		elif event.type == pygame.KEYDOWN:
-			check_keydown_events(event, paddle, ball, settings, stats, msg)
+			check_keydown_events(event, paddle, ball, bricks, screen, settings, sb, stats, msg)
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, paddle)
 
-def check_keydown_events(event, paddle, ball, settings, stats, msg):
+def check_keydown_events(event, paddle, ball, bricks, screen, settings, sb, stats, msg):
 	#Responses to keypresses
 	if event.key == pygame.K_RIGHT:
 		paddle.moving_right = True
@@ -25,8 +25,8 @@ def check_keydown_events(event, paddle, ball, settings, stats, msg):
 	elif event.key == pygame.K_SPACE:
 		stats.game_active = True
 		ball.moving = True
-	elif (stats.lives_left <= 0) and event.key == pygame.K_SPACE:
-		reset_game(bricks, settings, screen, sb, stats)
+		if stats.lives_left <= 0:
+			reset_game(bricks, settings, screen, sb, stats)
 	elif event.key == pygame.K_q: 
 		sys.exit()
 
@@ -47,20 +47,20 @@ def update_screen(settings, screen, paddle, ball, bricks, sb, stats, msg):
 	bricks.draw(screen)
 	
 	#Draw Start prompt if game is inactive
-	#Check if lives < 0 and game is not active, otherwise show the start
-	#prompt
-	if not stats.game_active and (stats.lives_left <= 0):
-		msg.show_game_over()
-		check_events(paddle, ball, settings, stats, msg)
-	elif not stats.game_active:
+	#Check if lives < 0 and game is not active. Draws game over message and allows player to restart if true
+	if not stats.game_active:
 		msg.show_start()
-		
+		if stats.lives_left <= 0:
+			msg.show_game_over()
+			check_events(paddle, ball, bricks, screen, settings, sb, stats, msg)
+			
 	#Make the most recently flipped screen visible.
 	pygame.display.flip()
 	
 """
 Purpose of build_wall():
-Creates a full wall of bricks, 9 columns by 8 rows.
+- Creates a full wall of bricks, 9 columns by 8 rows.
+- Wall is defined with constants stated in def build_wall()
 """	
 
 def build_wall(settings, screen, bricks):
@@ -88,7 +88,7 @@ def build_wall(settings, screen, bricks):
 Purpose of update_bricks():
 - Remove any brick that collides with the ball and add settings.brick_points 
 to the score for any brick hit.
-- Add the correct score if more than brick is hit.
+- Add the correct score if more than one brick is hit.
 - Creates a new wall if all of the bricks are destroyed.
 - Also increases the dynamic settings of the game (when all bricks are destroyed).
 """	
@@ -109,7 +109,7 @@ def update_bricks(bricks, ball, screen, settings, sb, stats):
 		stats.game_active = False
 		settings.increase_speed()
 		
-		#Increase level
+		#Increase level and make new level into an image
 		stats.level += 1
 		sb.prep_level()
 		
@@ -121,13 +121,8 @@ def check_hi_score(stats, sb):
 		stats.hi_score = stats.score
 		sb.prep_hi_score()
 	
-"""
-This is where I am currently trying to find a fix for when the lives run out.
-I want to display the game over message in this case, and the stats for score,
-level and lives should be reset should the player choose to start another game.
-"""
 def update_lives(paddle, ball, bricks, settings, screen, sb, stats):
-	#-1 to self.lives_left if ball touches bottom of screen
+	#Lose a life if ball touches bottom of screen
 	if ball.rect.bottom >= settings.screen_height:
 		stats.lives_left -= 1
 		sb.prep_info()
@@ -146,8 +141,12 @@ def reset_game(bricks, settings, screen, sb, stats):
 	stats.reset_stats()
 	settings.init_dynamic_settings()
 			
-	#Empty the rest of the bricks and create a new wall
+	#Remove the rest of the bricks and create a new wall
 	bricks.empty()
 	build_wall(settings, screen, bricks)
-			
+	
+	#Change the reset stats into images and draw them onto the screen
+	sb.prep_info()
+	sb.prep_level()
+	sb.show_scores()
 
