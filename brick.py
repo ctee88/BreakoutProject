6d4067d5c17571e90de002d8988ab767249650c6
ctee88 <61874412+ctee88@users.noreply.github.com>
2020-03-10 15:04:24 +0000

    Update brick.py

--- brick.py
+++ brick.py
@@ -1,17 +1,16 @@
 #Author: Cameron Tee
 #Date: 07/03/2020
-#Purpose: A class managing the behaviour of the bricks
+#Purpose: A class managing the properties of the bricks
 
 import pygame
 
 class Brick(pygame.sprite.Sprite):
 	
 	def __init__(self, settings, screen):
-		"""Initialize the brick and its starting position"""
+		#Initialize the brick image and get its rect attribute
 		super().__init__()
 		self.screen = screen
 		
-		#Load the brick image and get its rect attribute
 		self.image = pygame.image.load('images/brick_1.bmp')
 		self.rect = self.image.get_rect()
 	
